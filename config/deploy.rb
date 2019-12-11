f4052b86cb48e4fe93fec4a783a764e16ddf2c45
tabachain <tabata_katsutoshi@c-fo.com>
2019-12-11 22:58:19 +0900

    yarnで落ちる部分修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :linked_dirs, %w(log)
 
 set :deploy_to, "/home/ec2-user/test-deploy"
 
+set :log_level, :debug
 # 指定のディレクトリのシンボルを作成
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", '.bundle'
 
@@ -15,6 +16,17 @@ namespace :deploy do
     on roles(:app) do
       execute "sudo service nginx stop"
       execute "sudo service nginx start"
+    end
+  end
+
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync the assets'
+    task :precompile do
+      Rake.sh "npm install --prefix .local-yarn -g yarn@1.2"
+      Rake.sh '.local-yarn/bin/yarn install'
+      Rake.sh '.local-yarn/bin/yarn run build'
+      raise 'ERROR: yarn install or yarn run build failed at deploy:assets:precompile !' unless $?.success?
+      Rake.sh "RAILS_ENV=#{fetch(:rails_env)} bundle exec rake assets:precompile"
     end
   end
   after  :finished, :nginx_restart
